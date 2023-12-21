9abf0dac59d7f638c0fa027dda17575ea495abae
pobijunior <pobijunior@naver.com>
2023-12-21 20:54:38 +0900

    middle save

--- pohub/src/App.js
+++ pohub/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <aside className="sidebar">
+        <div className="logo-box">
+          <img src={Logo} alt='Nothing Here' />
+        </div>
+        <div className="thread-container">
+          <div className="thread-box">
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+            <div className="threadx" />
+          </div>
+        </div>
+        <div className="footer" />
+      </aside>
+      <div className='void' />
+      <main className="main">
+        <header className="header-container">
+          <div className="search-bar" />
+          <UserIcon className="user-icon" />
+        </header>
+        <section className="section-container">
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+          <div className="boardx" />
+        </section>
+      </main>
     </div>
-  );
+  )
+}
+
+function UserIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </svg>
+  )
 }
 
 export default App;
